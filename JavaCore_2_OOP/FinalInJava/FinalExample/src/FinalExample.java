0d8b39b650894e91dd120968d9d11dc37a99affb
= <=>
2021-03-05 18:32:09 +0700

    Exercise 6-OOP-Person

---
+++ JavaCore_2_OOP/FinalInJava/FinalExample/src/FinalExample.java
@@ -0,0 +1,17 @@
+/**
+ * ClassOne
+ */
+class FinalExample {
+    static final int MAX;
+
+    static{
+        MAX = 90;
+    }
+}
+
+    // public FinalExample() {
+    //     MAX_SPEED = 90;
+    //     System.out.println(MAX_SPEED);
+    // }
+ 
+    
