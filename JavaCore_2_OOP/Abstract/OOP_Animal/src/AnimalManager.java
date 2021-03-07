1c83637beec555e8d2d17b86a760e4e294985958
= <=>
2021-03-07 20:48:53 +0700

    Interface Example

--- JavaCore_2_OOP/Abstract/OOP_Animal/src/AnimalManager.java
+++ JavaCore_2_OOP/Abstract/OOP_Animal/src/AnimalManager.java
@@ -1,17 +1,17 @@
 public class AnimalManager {
     public static void main(String[] args) throws Exception {
         //Animal animal = new Animal();
-        Pets pets = new Pets();
+        // Pets pets = new Pets();
 
-        System.out.println("Nhap thong tin: ");
-        pets.input();
+        // System.out.println("Nhap thong tin: ");
+        // pets.input();
 
-        System.out.println("Thong tin vua nhap la: ");
-        pets.display();
+        // System.out.println("Thong tin vua nhap la: ");
+        // pets.display();
 
-        pets.eat("fish");
-        System.out.println("Toc do: " + pets.run(300));
+        // pets.eat("fish");
+        // System.out.println("Toc do: " + pets.run(300));
 
-        System.out.println("Thoi gian ngu moi ngay: " + pets.sleep(10));
+        // System.out.println("Thoi gian ngu moi ngay: " + pets.sleep(10));
     }
 }
---
+++ JavaCore_2_OOP/InterfaceExample/InterfaceExample/Interface_EmployeeManager/src/FullTimeEmployee.java
@@ -0,0 +1,16 @@
+/**
+ * FullTimeEmployee
+ */
+public class FullTimeEmployee extends Employee {
+
+    public FullTimeEmployee(String name, int paymentPerHour) {
+        super(name, paymentPerHour);
+    }
+
+    @Override
+    public int calculatorSalary() {
+        // TODO Auto-generated method stub
+        return 8 * getPaymentPerHour();
+    }
+
+}
